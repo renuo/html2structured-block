c69e33018e0e8ab0f532275c4cf5283265afb7e3
Pascal Andermatt <p.andermatt@me.com>
2023-03-21 18:05:41 +0100

    lint file

--- example/example.js
+++ example/example.js
@@ -1,68 +1,68 @@
-const { html2block, boolToDatoCMS, fetchRecords, uploadToDatoCMS } = require('../html2datocms.js');
-const { buildClient } = require('@datocms/cma-client-node');
+const { html2block, boolToDatoCMS, fetchRecords, uploadToDatoCMS } = require('../html2datocms.js')
+const { buildClient } = require('@datocms/cma-client-node')
 
 DATO_ITEM_TYPE_ID = 'YOUR_ITEM_TYPE_ID'
 DATO_IMG_BLOCK_ID = 'YOUR_IMG_BLOCK_ID'
 DATO_API_TOKEN = 'YOUR_API_TOKEN'
 
-async function example() {
-    const example_data = {
-        "title": "Example Title",
-        "active": "true",
-        "image": "https://picsum.photos/200/300",
-        // text with translations
-        "description": "<p><strong>Bold Text</strong>, <i>Italic Text</i> and <u>Underlined Text</u></p>",
-        "translations": {
-            "de-CH": {
-                "description": "<p><strong>Fett Text</strong>, <i>Kursiv Text</i> und <u>Unterstrichener Text</u></p>",
-            },
-        },
-        // text with images
-        "description_img": "<p><strong>Bold Text</strong>, <i>Italic Text</i> und <u>Underlined Text</u></p><img src=\"https://picsum.photos/200/300\" alt=\"Image Alt Text\" />",
-        "categories": ["Events", "News"]
-    };
+async function example () {
+  const exampleData = {
+    title: 'Example Title',
+    active: 'true',
+    image: 'https://picsum.photos/200/300',
+    // text with translations
+    description: '<p><strong>Bold Text</strong>, <i>Italic Text</i> and <u>Underlined Text</u></p>',
+    translations: {
+      'de-CH': {
+        description: '<p><strong>Fett Text</strong>, <i>Kursiv Text</i> und <u>Unterstrichener Text</u></p>'
+      }
+    },
+    // text with images
+    description_img: '<p><strong>Bold Text</strong>, <i>Italic Text</i> und <u>Underlined Text</u></p><img src="https://picsum.photos/200/300" alt="Image Alt Text" />',
+    categories: ['Events', 'News']
+  }
 
-    const client = buildClient({ apiToken: DATO_API_TOKEN });
+  const client = buildClient({ apiToken: DATO_API_TOKEN })
 
-    // check if item already exists (assuming title is unique)
-    var records = await fetchRecords("my_item", "title", example_data.title, client);
-    if (records.length > 0) {
-        console.log("Item already exists!");
-        return;
-    }
+  // check if item already exists (assuming title is unique)
+  const records = await fetchRecords('my_item', 'title', exampleData.title, client)
+  if (records.length > 0) {
+    console.log('Item already exists!')
+    return
+  }
 
-    dato_categories = []
-    for (let i = 0; i < example_data.categories.length; i++) {
-        const category = example_data.categories[i];
-        var records = await fetchRecords("category", "name", category, client);
+  datoCategories = []
+  for (let i = 0; i < exampleData.categories.length; i++) {
+    const category = exampleData.categories[i]
+    const records = await fetchRecords('category', 'name', category, client)
 
-        if (records.length > 0) dato_categories.push(records[0].id.toString());
-        else console.log("Category " + category + " not found!");
-    }
+    if (records.length > 0) datoCategories.push(records[0].id.toString())
+    else console.log('Category ' + category + ' not found!')
+  }
 
-    const dato_item = {
-        item_type: { type: 'item_type', id: DATO_ITEM_TYPE_ID },
-        title: example_data.title,
-        description: {
-            "en": await html2block(example_data.description),
-            "de-CH": await html2block(example_data.translations["de-CH"].description),
-        },
-        // client is needed for uploading images
-        description_img: await html2block(example_data.description_img, client, DATO_IMG_BLOCK_ID),
-        active: boolToDatoCMS(example_data.active),
-        image: await uploadToDatoCMS(example_data.image, client),
-        categories: dato_categories,
-    };
+  const datoItem = {
+    item_type: { type: 'item_type', id: DATO_ITEM_TYPE_ID },
+    title: exampleData.title,
+    description: {
+      en: await html2block(exampleData.description),
+      'de-CH': await html2block(exampleData.translations['de-CH'].description)
+    },
+    // client is needed for uploading images
+    description_img: await html2block(exampleData.description_img, client, DATO_IMG_BLOCK_ID),
+    active: boolToDatoCMS(exampleData.active),
+    image: await uploadToDatoCMS(exampleData.image, client),
+    categories: datoCategories
+  }
 
-    await client.items.create(dato_item).catch((e) => {
-        console.log(e);
+  await client.items.create(datoItem).catch((e) => {
+    console.log(e)
 
-        // To inspect the item if something is wrong
-        const fs = require('fs');
-        fs.writeFile('example_dato_item.json', JSON.stringify(dato_item), function () { });
+    // To inspect the item if something is wrong
+    const fs = require('fs')
+    fs.writeFile('example_datoItem.json', JSON.stringify(datoItem), function () { })
 
-        console.log("Error while creating item. Check example_dato_item.json");
-    });
+    console.log('Error while creating item. Check example_datoItem.json')
+  })
 }
 
-example();
+example()
--- html2datocms.js
+++ html2datocms.js
@@ -1,50 +1,50 @@
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require('jsdom')
 
-let root_structured_text_node = [];
-let client = null;
-let imageBlockId = null;
+let rootStructuredTextNode = []
+let client = null
+let imageBlockId = null
 
-async function buildTree(nodes, is_root = false) {
-  let structured_text_node = [];
-  if (is_root) structured_text_node = root_structured_text_node;
+async function buildTree (nodes, isRoot = false) {
+  let structuredTextNode = []
+  if (isRoot) structuredTextNode = rootStructuredTextNode
 
   for (let i = 0; i < nodes.length; i++) {
-    const child = nodes[i];
-    if (child.nodeName == 'P') {
-      res = await buildTree(child.childNodes);
+    const child = nodes[i]
+    if (child.nodeName === 'P') {
+      res = await buildTree(child.childNodes)
 
-      if (child.textContent.trim() == '') for (let j = 0; j < res.length; j++) structured_text_node.push(res[j]);
-      else structured_text_node.push({ type: 'paragraph', children: res });
+      if (child.textContent.trim() === '') for (let j = 0; j < res.length; j++) structuredTextNode.push(res[j])
+      else structuredTextNode.push({ type: 'paragraph', children: res })
     } else if (/^H[1-6]$/.test(child.nodeName)) {
-      if (child.textContent.trim() == '') continue;
+      if (child.textContent.trim() === '') continue
 
-      structured_text_node.push({
+      structuredTextNode.push({
         type: 'heading',
         level: parseInt(child.nodeName.replace('H', '')),
-        children: [{ type: 'span', marks: [], value: child.textContent.trim() }],
-      });
+        children: [{ type: 'span', marks: [], value: child.textContent.trim() }]
+      })
     } else if (/^(#text|SPAN|SUP|TT|FONT)$/.test(child.nodeName)) {
-      if (child.textContent.trim() == '') continue;
+      if (child.textContent.trim() === '') continue
 
-      child.textContent = child.textContent.replace(/(\r\n|\n|\r)/gm, '');
-      structured_text_node.push({
-        type: 'span', marks: [], value: child.textContent,
-      });
-    } else if (child.nodeName == 'A') {
-      structured_text_node.push({
+      child.textContent = child.textContent.replace(/(\r\n|\n|\r)/gm, '')
+      structuredTextNode.push({
+        type: 'span', marks: [], value: child.textContent
+      })
+    } else if (child.nodeName === 'A') {
+      structuredTextNode.push({
         type: 'link',
         url: child.href,
         children: [
           {
             type: 'span',
             marks: [],
-            value: child.textContent,
-          },
+            value: child.textContent
+          }
         ],
-        meta: [{ id: 'target', value: '_blank' }],
-      });
+        meta: [{ id: 'target', value: '_blank' }]
+      })
     } else if (/^(SMALL|STRONG|B|I|EM|U)$/.test(child.nodeName)) {
-      if (child.textContent.trim() == '') continue;
+      if (child.textContent.trim() === '') continue
 
       const mark = {
         SMALL: 'emphasis',
@@ -52,159 +52,159 @@ async function buildTree(nodes, is_root = false) {
         EM: 'emphasis',
         STRONG: 'strong',
         B: 'strong',
-        U: 'underline',
-      }[child.nodeName];
+        U: 'underline'
+      }[child.nodeName]
 
-      child.textContent = child.textContent.replace(/(\r\n|\n|\r)/gm, '');
-      structured_text_node.push({
+      child.textContent = child.textContent.replace(/(\r\n|\n|\r)/gm, '')
+      structuredTextNode.push({
         type: 'span',
         marks: [mark],
-        value: child.textContent,
-      });
-    } else if (child.nodeName == 'IMG') {
-      const uploadPhoto = await uploadToDatoCMS(child.src, client);
-      if (uploadPhoto == null) {
-        root_structured_text_node.push(await error_block(`Image upload failed: ${image_url}`));
-        continue;
+        value: child.textContent
+      })
+    } else if (child.nodeName === 'IMG') {
+      const uploadPhoto = await uploadToDatoCMS(child.src, client)
+      if (uploadPhoto === null) {
+        rootStructuredTextNode.push(await errorBlock(`Image upload failed: ${imageUrl}`))
+        continue
       }
-      root_structured_text_node.push({
+      rootStructuredTextNode.push({
         item: {
           type: 'item',
           attributes: {
-            image: uploadPhoto,
+            image: uploadPhoto
           },
-          relationships: { item_type: { data: { id: imageBlockId, type: 'item_type' } } },
+          relationships: { item_type: { data: { id: imageBlockId, type: 'item_type' } } }
         },
-        type: 'block',
-      });
-    } else if (child.nodeName == 'UL' || child.nodeName == 'OL') {
-      const style = child.nodeName == 'OL' ? 'numbered' : 'bulleted';
-      structured_text_node.push({
+        type: 'block'
+      })
+    } else if (child.nodeName === 'UL' || child.nodeName === 'OL') {
+      const style = child.nodeName === 'OL' ? 'numbered' : 'bulleted'
+      structuredTextNode.push({
         type: 'list',
         children: await buildTree(child.childNodes),
-        style,
-      });
-    } else if (child.nodeName == 'LI') {
-      structured_text_node.push({
+        style
+      })
+    } else if (child.nodeName === 'LI') {
+      structuredTextNode.push({
         type: 'listItem',
         children: [
           {
             children: await buildTree(child.childNodes),
-            type: 'paragraph',
-          },
-        ],
-      });
-    } else if (child.nodeName == 'CODE' || child.nodeName == 'PRE') {
-      root_structured_text_node.push({
+            type: 'paragraph'
+          }
+        ]
+      })
+    } else if (child.nodeName === 'CODE' || child.nodeName === 'PRE') {
+      rootStructuredTextNode.push({
         code: child.textContent,
-        type: 'code',
-      });
-    } else if (child.nodeName == 'HR') {
-      structured_text_node.push({ type: 'thematicBreak' });
-    } else if (child.nodeName == 'BLOCKQUOTE') {
-      const bC = await buildTree(child.childNodes);
-      if (bC.length == 0) continue;
-      structured_text_node.push({
+        type: 'code'
+      })
+    } else if (child.nodeName === 'HR') {
+      structuredTextNode.push({ type: 'thematicBreak' })
+    } else if (child.nodeName === 'BLOCKQUOTE') {
+      const bC = await buildTree(child.childNodes)
+      if (bC.length === 0) continue
+      structuredTextNode.push({
         type: 'blockquote',
-        children: [{ children: bC, type: 'paragraph' }],
-      });
-    } else if (child.nodeName == 'ASIDE') {
-      structured_text_node.push({
+        children: [{ children: bC, type: 'paragraph' }]
+      })
+    } else if (child.nodeName === 'ASIDE') {
+      structuredTextNode.push({
         type: 'paragraph',
-        children: await buildTree(child.childNodes),
-      });
+        children: await buildTree(child.childNodes)
+      })
     } else {
-      structured_text_node.push(await error_block(`Unknown node: ${child.nodeName}`));
+      structuredTextNode.push(await errorBlock(`Unknown node: ${child.nodeName}`))
     }
 
-    const last_item = structured_text_node.length - 1;
-    const non_root_blocks = ['span', 'link'];
-    if (last_item >= 0 && is_root && non_root_blocks.includes(structured_text_node[last_item].type)) {
-      structured_text_node.push({
+    const lastItem = structuredTextNode.length - 1
+    const nonRootBlocks = ['span', 'link']
+    if (lastItem >= 0 && isRoot && nonRootBlocks.includes(structuredTextNode[lastItem].type)) {
+      structuredTextNode.push({
         type: 'paragraph',
-        children: [structured_text_node[last_item]],
-      });
-      structured_text_node.splice(last_item, 1);
+        children: [structuredTextNode[lastItem]]
+      })
+      structuredTextNode.splice(lastItem, 1)
     }
   }
-  return structured_text_node;
+  return structuredTextNode
 }
 
-async function error_block(error) {
-  error = `${error.substring(0, 500)}...`;
-  console.log(`!!!!!!!! Error !!!!!!!! ${error}`);
+async function errorBlock (error) {
+  error = `${error.substring(0, 500)}...`
+  console.log(`!!!!!!!! Error !!!!!!!! ${error}`)
   return {
     type: 'paragraph',
-    children: [{ type: 'span', marks: ['strong'], value: `!!!!!!!! Error !!!!!!!! ${error}` }],
-  };
+    children: [{ type: 'span', marks: ['strong'], value: `!!!!!!!! Error !!!!!!!! ${error}` }]
+  }
 }
 
-async function html2block(html, c = null, img = null) {
-  if (!html) return { schema: 'dast', document: { type: 'root', children: [] } };
+async function html2block (html, c = null, img = null) {
+  if (!html) return { schema: 'dast', document: { type: 'root', children: [] } }
 
-  client = c;
-  imageBlockId = img;
-  root_structured_text_node = [];
-  elements_to_remove = ['div', 'main', 'br'];
-  for (let i = 0; i < elements_to_remove.length; i++) {
-    const element = elements_to_remove[i];
-    html = html.replace(new RegExp(`<${element}.*?>`, 'g'), '');
-    html = html.replace(new RegExp(`</${element}>`, 'g'), '');
+  client = c
+  imageBlockId = img
+  rootStructuredTextNode = []
+  elementsToRemove = ['div', 'main', 'br']
+  for (let i = 0; i < elementsToRemove.length; i++) {
+    const element = elementsToRemove[i]
+    html = html.replace(new RegExp(`<${element}.*?>`, 'g'), '')
+    html = html.replace(new RegExp(`</${element}>`, 'g'), '')
   }
-  const htmls = html.split('\n');
+  const htmls = html.split('\n')
 
-  new_html = '';
+  newHtml = ''
   for (let i = 0; i < htmls.length; i++) {
-    const line = htmls[i];
-    if (line.trim() == '') continue;
+    const line = htmls[i]
+    if (line.trim() === '') continue
     if (line.startsWith('<') && line.endsWith('>')) {
-      new_html += line;
-      continue;
+      newHtml += line
+      continue
     }
-    new_html = `${new_html}<p>${line}</p>`;
+    newHtml = `${newHtml}<p>${line}</p>`
   }
 
-  const dom = new JSDOM(`<!DOCTYPE html><body>${new_html}</body>`);
-  const nodes = dom.window.document.body.childNodes;
+  const dom = new JSDOM(`<!DOCTYPE html><body>${newHtml}</body>`)
+  const nodes = dom.window.document.body.childNodes
 
-  return { schema: 'dast', document: { type: 'root', children: await buildTree(nodes, true) } };
+  return { schema: 'dast', document: { type: 'root', children: await buildTree(nodes, true) } }
 }
 
-async function uploadToDatoCMS(image_url, client) {
+async function uploadToDatoCMS (imageUrl, client) {
   try {
     const uploadPhoto = await client.uploads.createFromUrl({
-      url: image_url,
-      skipCreationIfAlreadyExists: true,
+      url: imageUrl,
+      skipCreationIfAlreadyExists: true
     }).catch((e) => {
-      console.log(e);
-    });
+      console.log(e)
+    })
     return {
-      upload_id: uploadPhoto.id,
-    };
+      upload_id: uploadPhoto.id
+    }
   } catch (e) {
-    console.log(e);
-    return null;
+    console.log(e)
+    return null
   }
 }
 
-function boolToDatoCMS(bool) {
-  return bool == 'true';
+function boolToDatoCMS (bool) {
+  return bool === 'true'
 }
 
-async function fetchRecords(type, field, value, client) {
+async function fetchRecords (type, field, value, client) {
   const records = await client.items.list({
     filter: {
       type,
-      fields: { [field]: { eq: value } },
+      fields: { [field]: { eq: value } }
     },
     page: { limit: 100 },
-    version: 'current',
-  });
+    version: 'current'
+  })
 
-  return records;
+  return records
 }
 
-exports.html2block = html2block;
-exports.uploadToDatoCMS = uploadToDatoCMS;
-exports.fetchRecords = fetchRecords;
-exports.boolToDatoCMS = boolToDatoCMS;
+exports.html2block = html2block
+exports.uploadToDatoCMS = uploadToDatoCMS
+exports.fetchRecords = fetchRecords
+exports.boolToDatoCMS = boolToDatoCMS
